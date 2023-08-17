1ec8ebfce9e2850df920e554b48af4d3e011f61a
Replit user <>
2023-08-17 20:29:30 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -12,9 +12,8 @@ y su tamaño, y devuelva el valor más grande del arreglo.
 int findMax(int arr[], int size) {
   int maxNum = arr[0];
   for (int i = 0; i < size; i++) {
-
-    if (maxNum < arr[i + 1]) {
-      maxNum = arr[i + 1];
+    if (maxNum < arr[i]) {
+      maxNum = arr[i];
     }
   }
 
